8f00451a26df7191fdf965e0697887c61845d3d1
vapaus <snaqerou@gmail.com>
2020-10-26 21:09:10 +0100

    added standard grade book class

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(String name) : base(name)
+        {
+            this.Type = Enums.GradeBookType.Standard;
+        }
+    }
+}
